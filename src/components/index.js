e2363f25a84a2dcaeefcb8415cf6e0c2940e957a
premnarayanp <premnarayanpatel223@gmail.com>
2024-08-15 02:31:45 +0530

    Project structure and required modeule installed

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import ArrivalsItemCard from './ArrivalsItemCard';
+import ProductCard from './ProductCard';
+import HeaderCard from './HeaderCard';
+import SortingAndFilter from './SortingAndFilter';
+export { ArrivalsItemCard, ProductCard, HeaderCard, SortingAndFilter };
