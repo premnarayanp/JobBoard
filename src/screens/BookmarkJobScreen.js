91e7900c257f632d45a1a105d352873f44fb8ef7
premnarayanp <premnarayanpatel223@gmail.com>
2024-08-15 13:30:15 +0530

    projects completed

--- src/screens/BookmarkJobScreen.js
+++ src/screens/BookmarkJobScreen.js
@@ -1,9 +1,33 @@
-import { View, Text } from 'react-native';
 
-export default function BookmarkJobScreen() {
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { JobCard } from '../components/index';
+
+const BookmarkJobScreen = ({ navigation }) => {
+    const [bookmarks, setBookmarks] = useState([]);
+
+    useEffect(() => {
+        const fetchBookmarks = async () => {
+            const storedBookmarks = await AsyncStorage.getItem('bookmarks');
+            setBookmarks(storedBookmarks ? JSON.parse(storedBookmarks) : []);
+        };
+        fetchBookmarks();
+    }, []);
+
+
+
     return (
-        <View>
-            <Text>JobScreen</Text>
+        <View style={{ flex: 1, padding: 10 }}>
+            <FlatList
+                data={bookmarks}
+                renderItem={({ item }) => <JobCard item={item} navigation={navigation} />}
+                keyExtractor={(item) => item.id + item.type}
+                onEndReached={() => setPage(1)}
+                onEndReachedThreshold={0.5}
+            />
         </View>
     );
-}
+};
+
+export default BookmarkJobScreen;
